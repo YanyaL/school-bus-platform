d4f86a55aa59149956a0d307fa5f76def79a3203
YanyaL <s4789503@student.uq.edu.au>
2026-07-31 17:50:17 +1000

    账号功能链路1

---
+++ src/main/java/com/schoolbus/iam/domain/account/AccountStatus.java
@@ -0,0 +1,6 @@
+package com.schoolbus.iam.domain.account;
+
+public enum AccountStatus {
+    ACTIVE,
+    DISABLED
+}
---
+++ src/main/java/com/schoolbus/iam/domain/account/Role.java
@@ -0,0 +1,6 @@
+package com.schoolbus.iam.domain.account;
+
+public enum Role {
+    STUDENT,
+    ADMIN
+}
